2b10fe6745be82a5c56579fb97f6c3cb05a366dc
Josafá Luna <josafaluna@gmail.com>
2021-11-01 18:49:43 -0300

    Developed ist all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,7 +1,11 @@
 package one.digitalinnovation.personapi.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
@@ -22,6 +26,7 @@ public class PersonService {
 		this.repository = personRepository;
 	}
 
+	@Transactional
 	public MessageResponseDTO createPerson(PersonDTO personDTO) {
 
 		Person personToSave = personMapper.toModel(personDTO);
@@ -29,6 +34,17 @@ public class PersonService {
 		Person savedPerson = repository.save(personToSave);
 
 		return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
+	}
+
+	@Transactional(readOnly = true)
+	public List<PersonDTO> findAll() {
+		List<Person> allPerson = repository.findAll();
+		// eu criei assim
+		// return allPerson.stream().map(p ->
+		// personMapper.toDTO(p)).collect(Collectors.toList());
+
+		// o professor criou assim
+		return allPerson.stream().map(personMapper::toDTO).collect(Collectors.toList());
 	}
 
 }
